b6b96bd51a3ad3e021cfc34d2e831818c12dbe65
Gorush116 <gorush34@gmail.com>
2024-01-30 08:59:04 +0900

    property setting

---
+++ src/main/java/com/example/property/controller/HttpController.java
@@ -0,0 +1,25 @@
+package com.example.property.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@RequiredArgsConstructor
+@Controller
+public class HttpController {
+
+//    @GetMapping("/testJs")
+//    public ModelAndView testJs(HttpServletRequest request, HttpServletResponse response) {
+//
+//        ModelAndView mav = new ModelAndView();
+//
+//        mav.setViewName("testJs");
+//
+//        return mav;
+//
+//    }
+
+}
